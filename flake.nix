f2b5cba8063f8e867a15d568fff16f6742e53522
kodaf <contact@kodaf.me>
2025-10-05 11:30:47 +0300

    remove nvf & php

--- flake.nix
+++ flake.nix
@@ -6,11 +6,6 @@
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
-
-    nvf = {
-      url = "github:NotAShelf/nvf";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs @ {
@@ -18,7 +13,6 @@
     nix-darwin,
     nixpkgs,
     nix-homebrew,
-    nvf,
   }: let
     configuration = {pkgs, ...}: {
       homebrew = {
@@ -55,132 +49,8 @@
         go
         htop
         nodejs
-        php
         tmux
       ];
-      programs.nvf = {
-        enable = true;
-
-        settings.vim = {
-          viAlias = true;
-          vimAlias = true;
-
-          options = {
-            shiftwidth = 4;
-            tabstop = 4;
-          };
-
-          lsp = {
-            enable = true;
-            formatOnSave = true;
-            inlayHints.enable = true;
-            lightbulb.enable = true;
-            lightbulb.autocmd.enable = true;
-          };
-
-          languages = {
-            enableFormat = true;
-            enableTreesitter = true;
-
-            nix.enable = true;
-            markdown.enable = true;
-            bash.enable = true;
-            css.enable = true;
-            html.enable = true;
-            sql.enable = true;
-            ts.enable = true;
-            go.enable = true;
-            lua.enable = true;
-            python.enable = true;
-            php = {
-              enable = true;
-              lsp = {
-                enable = true;
-                server = "intelephense";
-              };
-            };
-            tailwind.enable = true;
-          };
-
-          treesitter = {
-            indent.enable = false;
-          };
-
-          filetree = {
-            neo-tree = {
-              enable = true;
-            };
-          };
-
-          binds = {
-            cheatsheet.enable = true;
-            whichKey.enable = true;
-          };
-
-          visuals = {
-            nvim-scrollbar.enable = true;
-            nvim-web-devicons.enable = true;
-            nvim-cursorline.enable = true;
-            cinnamon-nvim.enable = true;
-            fidget-nvim.enable = true;
-
-            highlight-undo.enable = true;
-            indent-blankline.enable = true;
-
-            # Fun
-            cellular-automaton.enable = false;
-          };
-
-          statusline = {
-            lualine = {
-              enable = true;
-              theme = "catppuccin";
-            };
-          };
-
-          theme = {
-            enable = true;
-            name = "catppuccin";
-            style = "mocha";
-            transparent = false;
-          };
-
-          autocomplete = {
-            nvim-cmp.enable = true;
-          };
-
-          telescope.enable = true;
-
-          git = {
-            enable = true;
-            gitsigns.enable = true;
-            gitsigns.codeActions.enable = false; # throws an annoying debug message
-          };
-
-          ui = {
-            borders.enable = true;
-            noice.enable = true;
-            colorizer.enable = true;
-            modes-nvim.enable = false; # the theme looks terrible with catppuccin
-            illuminate.enable = true;
-            breadcrumbs = {
-              enable = true;
-              navbuddy.enable = true;
-            };
-            smartcolumn = {
-              enable = true;
-              setupOpts.custom_colorcolumn = {
-                # this is a freeform module, it's `buftype = int;` for configuring column position
-                nix = "110";
-                ruby = "120";
-                java = "130";
-                go = ["90" "130"];
-              };
-            };
-            fastaction.enable = true;
-          };
-        };
-      };
     };
   in {
     # Build darwin flake using:
@@ -188,7 +58,6 @@
     darwinConfigurations."macos" = nix-darwin.lib.darwinSystem {
       modules = [
         configuration
-        nvf.nixosModules.default
       ];
     };
   };
